2fa71de1f3bf52326f9b009b524108835a32413f
Syc056 <92353611+Syc056@users.noreply.github.com>
2022-01-06 09:24:23 +0900

    Update filter_coco.py

--- PM dectect/filter_coco.py
+++ PM dectect/filter_coco.py
@@ -1,6 +1,8 @@
 import fiftyone as fo
 import fiftyone.zoo as foz
 
+
+# Load the coco-2017 dataset and assign the class that I want to filtering.
 dataset = foz.load_zoo_dataset(
     "coco-2017",
     splits=["validation","train"],
@@ -14,8 +16,12 @@ session = fo.launch_app(dataset)
 export_dir = "C:/Users/sy__c/Desktop/test/filtered_data"
 label_field = "ground_truth"
 
+
+# Export the data that filtered with fiftyone and assign dataset type to YOLOv5 type 
 dataset.export(
     export_dir=export_dir,
     dataset_type=fo.types.YOLOv5Dataset,
     label_field=label_field
 )
+
+# filter the coco dataset with fiftyone 
