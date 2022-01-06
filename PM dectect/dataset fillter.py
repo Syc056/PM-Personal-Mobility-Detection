60034370de096f2d9d11f5d5e1eef6cd256680aa
Syc056 <92353611+Syc056@users.noreply.github.com>
2022-01-06 09:19:47 +0900

    Update dataset fillter.py

--- PM dectect/dataset fillter.py
+++ PM dectect/dataset fillter.py
@@ -3,8 +3,7 @@ import json
 with open('sample.json', 'r') as f:
 content = json.load(f)
 
-api_list = [ x for x in content['apistats']['2512'].keys()]
+api_list = [ x for x in content['person']['??'].keys()]
 
 print(api_list)
 
-출처: https://hashcode.tistory.com/entry/json-파일에서-특정-데이터를-추출하기 [hashcode]
